128a0da8132be9446f076b91a72de652f4073e64
r47717 <mchr@yandex.ru>
2015-12-08 19:41:51 +0300

    started debugging

--- cstrings.c
+++ cstrings.c
@@ -1,7 +1,11 @@
+#include <stdio.h>
+
+
 /**
 * Implementation of some string.h functions and some other custom functions
 * (c) r47717
 */
+
 
 /************************* some string.h functions ***************************/
 
@@ -12,8 +16,8 @@ int strcmp(const char *s1, const char *s2);
 int strncmp(const char *str1, const char *str2, size_t n);
 void strcpy(char *s1, const char *s2);
 void strncpy(char *s1, const char *s2, size_t n);
-char* strcat(char *dest, const char *src); 
-char* strncat(char *dest, const char *src, size_t n); 
+char* strcat(char *dest, const char *src);
+char* strncat(char *dest, const char *src, size_t n);
 char* strchr(const char *str, int c); /* Searches for the first occurrence of the character c */
 char* strstr(const char *haystack, const char *needle); /* Finds the first occurrence of the entire string needle */
 char* strtoupper(char *str);
@@ -21,7 +25,7 @@ char* strtolower(char *str);
 
 /************************* custom string functions ***************************/
 
-char* strcut(char *from, char *str); /* cuts str substring out from 'from' */ 
+char* strcut(char *from, char *str); /* cuts str substring out from 'from' */
 char* strcutch(char *from, char c); /* cuts all occurrences of character 'c'from string 'from' */
 char* strreplace(char *str, const char *from, const char *to); /* replaces all occurrences of 'from' with 'to' in 'str' */
 char* strrev(char *str); /* reverses the string */
@@ -30,28 +34,37 @@ char* strrtrim(char *str); /* removes whitespaces on the right */
 char* strltrim(char *str); /* removes whitespaces on the left */
 char* strcompact(char *str); /* reduces whilespace sequences inside string */
 
+
 /************************* string.h implementation ***************************/
 
 int strlen(const char *str) {
-	char *tmp = str;
-	while(*tmp++) {}
+	const char *tmp = str;
+	while (*tmp) {
+		tmp++;
+	}
 	return tmp - str;
 }
 
 int strcmp(const char *s1, const char *s2) {
-	while(*s1 && *s2) {
-		if(*s1 > *s2) return 1;
-		if(*s1 < *s2) return -1;
+	while (*s1 && *s2) {
+		if (*s1 > *s2) return 1;
+		if (*s1 < *s2) return -1;
 		s1++;
 		s2++;
 	}
+
+	if (*s1 && !*s2)
+		return 1;
+	if (!*s1 && *s2)
+		return -1;
+
 	return 0;
 }
 
-int strncmp(const char *str1, const char *str2, size_t n) {
-	while(*s1 && *s2 && n--) {
-		if(*s1 > *s2) return 1;
-		if(*s1 < *s2) return -1;
+int strncmp(const char *s1, const char *s2, size_t n) {
+	while (*s1 && *s2 && n--) {
+		if (*s1 > *s2) return 1;
+		if (*s1 < *s2) return -1;
 		s1++;
 		s2++;
 	}
@@ -59,12 +72,12 @@ int strncmp(const char *str1, const char *str2, size_t n) {
 }
 
 void strcpy(char *s1, char *s2) {
-	while(*s2)
+	while (*s2)
 		*s1++ = *s2++;
 }
 
 void strncpy(char *s1, char *s2, size_t n) {
-	while(n) {
+	while (n) {
 		*s1++ = *s2++;
 		n--;
 	}
@@ -73,43 +86,46 @@ void strncpy(char *s1, char *s2, size_t n) {
 
 char *strcat(char *dest, const char *src) {
 	char *res = dest;
-	while(*dest++) {}
-	while(*src) *dest++ = *src++;
+	while (*dest++) {}
+	while (*src) *dest++ = *src++;
 	*dest = 0;
 	return res;
 }
 
 char *strncat(char *dest, const char *src, size_t n) {
 	char *res = dest;
-	while(*dest++) {}
-	while(*src && n--) *dest++ = *src++;
+	while (*dest++) {}
+	while (*src && n--) *dest++ = *src++;
 	*dest = 0;
 	return res;
 }
 
 char *strchr(const char *str, int c) { /* Searches for the first occurrence of the character c */
-	while(*str) {
-		if(*str == c) return str;
+	while (*str) {
+		if (*str == c) return (char*)str;
 		str++;
 	}
+
+	return NULL;
 }
 
 char *strstr(const char *haystack, const char *needle) { /* Finds the first occurrence of the entire string needle */
-	while(*haystack) {
-		char *hay = haystack;
-		char *need == needle;
-		while(*need++ == *hay++) 
-		{}
-		if(*need == 0)
+	while (*haystack) {
+		const char *hay = haystack;
+		const char *need = needle;
+		while (*need++ == *hay++)
+		{
+		}
+		if (*need == 0)
 			break;
 	}
-	return haystack;
+	return (char*)haystack;
 }
 
 char* strtoupper(char *str) {
 	char *tmp = str;
-	while(*tmp) {
-		if(*tmp >= 'a' && *tmp <= 'z')
+	while (*tmp) {
+		if (*tmp >= 'a' && *tmp <= 'z')
 			*tmp += 'A' - 'a';
 		tmp++;
 	}
@@ -118,8 +134,8 @@ char* strtoupper(char *str) {
 
 char* strtolower(char *str) {
 	char *tmp = str;
-	while(*tmp) {
-		if(*tmp >= 'A' && *tmp <= 'Z')
+	while (*tmp) {
+		if (*tmp >= 'A' && *tmp <= 'Z')
 			*tmp -= 'A' - 'a';
 		tmp++;
 	}
@@ -132,16 +148,16 @@ static int iswhitespace(char c) {
 	return c == ' ' || c == '\t' || c == '\n' || c == '\r';
 }
 
-char* strcut(char *from, char *str) { /* cuts str substring out from 'from' */ 
-	return strreplace(from, "");
+char* strcut(char *from, char *str) { /* cuts str substring out from 'from' */
+	return strreplace(from, str, "");
 }
 
-char* strcutch(char *from, char c) { /* cuts all occurrences of character 'c'from string 'from' */
+char* strcutch(char *str, char c) { /* cuts all occurrences of character 'c'from string 'str' */
 	char *save = str;
 	char *from = str;
-	char *to = str;
-	while(*from) {
-		if(*from == c)
+	char *to = from;
+	while (*from) {
+		if (*from == c)
 			from++;
 		else
 			*to++ = *from++;
@@ -151,22 +167,37 @@ char* strcutch(char *from, char c) { /* cuts all occurrences of character 'c'fro
 }
 
 char* strreplace(char *str, const char *from, const char *to) { /* replaces all occurrences of 'from' with 'to' in 'str' */
-	char *p1 = str;
-	char *p2 = str;
-	while(*p2) {
-		
+	char *newstr = str;
+	char *oldstr = str;
+	char to_len = strlen(to);
+	while (*oldstr) {
+		const char *fromtmp = from;
+		char *oldtmp = oldstr;
+		while (*oldtmp == *fromtmp && *fromtmp != 0) {
+			oldtmp++;
+			fromtmp++;
+		}
+		if (*fromtmp == 0) { /* 'form' string found */
+			newstr = strcat(newstr, to) + to_len;
+		}
+		else { /* not found */
+			do {
+				*newstr++ = *oldstr++;
+			} while (oldstr != oldtmp);
+		}
 	}
+	return str;
 }
 
 char* strrev(char *str) { /* reverses the string */
 	char *start = str;
 	char *end = str;
-	
-	if(*str == 0)
+
+	if (*str == 0)
 		return str;
-	while(*end) end++;
+	while (*end) end++;
 	end--;
-	while(start < end) {
+	while (start < end) {
 		char c;
 		c = *start;
 		*start = *end;
@@ -178,22 +209,23 @@ char* strrev(char *str) { /* reverses the string */
 
 char* strtrim(char *str) { /* removes whitespaces */
 	strltrim(str);
-	strrtrim(str);
+	return strrtrim(str);
 }
 
 char* strrtrim(char *str) { /* removes whitespaces on the right */
 	char *tmp = str;
-	if(*str == 0)
+	if (*str == 0)
 		return str;
-	while(*tmp) 
+	while (*tmp)
 		tmp++;
-	while(1) {
-		if(iswhitespace(*--tmp)) {
-			if(tmp == str) {
+	while (1) {
+		if (iswhitespace(*--tmp)) {
+			if (tmp == str) {
 				*tmp = 0;
 				break;
 			}
-		} else {
+		}
+		else {
 			tmp[1] = 0;
 			break;
 		}
@@ -205,8 +237,8 @@ char* strltrim(char *str) { /* removes whitespaces on the left */
 	char *save = str;
 	char *from = str;
 	char *to = str;
-	while(*from) {
-		if(iswhitespace(*from))
+	while (*from) {
+		if (iswhitespace(*from))
 			from++;
 		else
 			*to++ = *from++;
@@ -219,16 +251,17 @@ char* strcompact(char *str) { /* reduces whilespace sequences inside string */
 	char *from = str;
 	char *to = str;
 	int ws = 0;
-	
-	while(*from) {
-		if(iswhitespace(*tmp) {
-			if(ws)
+
+	while (*from) {
+		if (iswhitespace(*from)) {
+			if (ws)
 				from++;
 			else {
 				*to++ = *from++;
 				ws = 1;
 			}
-		} else
+		}
+		else
 			*to++ = *from++;
 	}
 	*to = 0;
@@ -236,4 +269,80 @@ char* strcompact(char *str) { /* reduces whilespace sequences inside string */
 }
 
 
+/************************** TESTS ******************************/
 
+int tests_run = 0;
+int tests_passed = 0;
+int tests_failed = 0;
+
+int test(char * descr, int value) {
+	tests_run++;
+	if (!value) {
+		printf("Test '%s' failed\n", descr);
+		tests_failed++;
+	}
+	else {
+		printf("Test '%s' successful\n", descr);
+		tests_passed++;
+	}
+
+	return value;
+}
+
+
+int main()
+{
+	/*int strlen(const char *str);*/
+
+	test("strlen 1", strlen("Hello") == 5);
+	test("strlen 2", strlen("") == 0);
+	test("strlen 3", strlen("werjk345348u9rtjk  owerwer89 897 we%^%@^#%^ dasd") == 48);
+
+	/*int strcmp(const char *s1, const char *s2);*/
+
+	test("strcmp 1", strcmp("hello", "hello 2") == -1);
+	test("strcmp 2", strcmp("", "hello") == -1);
+	test("strcmp 3", strcmp("hello 2", "hello 2") == 0);
+	test("strcmp 4", strcmp("hello 2", "hello") == 1);
+	test("strcmp 5", strcmp("absde", "abrerer") == 1);
+	test("strcmp 6", strcmp("absde", "") == 1);
+	test("strcmp 7", strcmp("", "") == 0);
+
+
+	/*int strncmp(const char *str1, const char *str2, size_t n);*/
+
+
+	/*void strcpy(char *s1, const char *s2);*/
+	
+	
+	/*void strncpy(char *s1, const char *s2, size_t n);*/
+	
+	
+	/*char* strcat(char *dest, const char *src);*/
+	
+	
+	/*char* strncat(char *dest, const char *src, size_t n);*/
+	
+	
+	/*char* strchr(const char *str, int c);*/ /* Searches for the first occurrence of the character c */
+	
+											  
+	/*char* strstr(const char *haystack, const char *needle);*/ /* Finds the first occurrence of the entire string needle */
+	
+																
+	/*char* strtoupper(char *str);*/
+	
+	
+	/*char* strtolower(char *str);*/
+
+
+
+	printf("\nTesting summary:\n");
+	printf("\tTests run: %d\n", tests_run);
+	printf("\tTests failed: %d\n", tests_failed);
+	printf("\tTests passed: %d\n", tests_passed);
+	if (tests_failed == 0)
+		printf("ALL TESTS PASSED\n");
+
+	return tests_failed == 0;
+}
